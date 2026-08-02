8c28caf980083b5b9692580ae870137ff00cedcf
RAVI LAKSHMAN <2400030658@kluniversity.in>
2026-08-02 22:35:03 +0530

    Refactor stoneGame method to comment out logic
    Commented out the original logic for the stoneGame method and returned true instead.

---
+++ Day-60/Solution877.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // boolean is1 = false;
+        // boolean is2 = false;
+        // int n = piles.length;
+
+        // int alica = 0;
+        // int bob = 0;
+
+        // int l = 0;
+        // int r = 0;
+        // while (l < r) {
+        //     int m1 = piles[l];
+        //     int m2 = piles[r];
+        //     if (m1 <= m2 && is1) {
+        //         alica += m1;
+        //         is2 = true;
+        //         is1 = false;
+        //         l++;
+        //     } else if (m2 >= m1 && is2) {
+        //         bob += m2;
+        //         is2 = false;
+        //         is1 = true;
+        //         r++;
+        //     }
+        // }
+        // return alica >= bob;
+
+
+      return true;
+    }
+}
