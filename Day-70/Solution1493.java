5b35299ff81adb5de545e5771f7bbaf0da1dba8b
RAVI LAKSHMAN <2400030658@kluniversity.in>
2026-08-12 22:41:21 +0530

    Implement longestSubarray method in Solution1493

---
+++ Day-70/Solution1493.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int n=nums.length;
+        int zeroc=0;
+        int onec=0;
+        int left=0;
+        int max=0;
+        for(int r=0;r<n;r++){
+            if (nums[r] == 0)
+                zeroc++;
+
+            while (zeroc > 1) {
+
+                if (nums[left] == 0)
+                    zeroc--;
+
+                left++;
+            }
+
+            max = Math.max(max, r - left );
+        }
+        return max;
+    }
+}
