b358b028b0653127a0af48d85f2407096f168deb
RAVI LAKSHMAN <2400030658@kluniversity.in>
2026-09-19 22:09:11 +0530

    Implement maximizeExpressionOfThree method

---
+++ Day-108/Solution3745.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int min=nums[0];
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int n=nums.length;
+
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>max1){
+                max2=max1;
+                max1=nums[i];
+            }
+            else if(nums[i]>max2 && nums[i]<=max1){
+                max2=nums[i];
+            }
+
+            if(nums[i]<min){
+                min=nums[i];
+            }
+        }
+
+
+        return max1+max2-min;
+
+    }
+}
